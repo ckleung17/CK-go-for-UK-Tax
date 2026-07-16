7aceb22a1dcf92d83de5af0b616d8cebe32886ab
CK Leung <ckleung17@hotmail.com>
2026-07-16 15:33:52 +0100

    SDLT example B updated

--- scripts/layout.js
+++ scripts/layout.js
@@ -3,6 +3,14 @@ const footerUrl = layoutScriptUrl
   ? new URL('../footer.html', layoutScriptUrl)
   : 'footer.html';
 
+if (layoutScriptUrl && footerUrl instanceof URL) {
+  const assetVersion = new URL(layoutScriptUrl).searchParams.get('v');
+
+  if (assetVersion) {
+    footerUrl.searchParams.set('v', assetVersion);
+  }
+}
+
 fetch(footerUrl)
   .then(response => response.text())
   .then(data => {
