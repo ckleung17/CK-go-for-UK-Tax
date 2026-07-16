2657428d39a46924a44041ca0d5f2fe53812e0c8
CK Leung <ckleung17@hotmail.com>
2026-07-16 08:41:13 +0100

    fix(layout): restore shared site assets

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,5 +1,10 @@
-fetch('../footer.html')
+const layoutScriptUrl = document.currentScript?.src;
+const footerUrl = layoutScriptUrl
+  ? new URL('../footer.html', layoutScriptUrl)
+  : 'footer.html';
+
+fetch(footerUrl)
   .then(response => response.text())
   .then(data => {
     document.getElementById('global-footer').innerHTML = data;
-  }); 
+  });
